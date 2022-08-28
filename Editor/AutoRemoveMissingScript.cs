22c3e21660db1bf500c41c634a52b6c372979dc7
baba-s <shotababa1206@gmail.com>
2022-08-28 16:56:52 +0900

    add

---
+++ Editor/AutoRemoveMissingScript.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// シーンを保存する時や Unity を再生する時に Missing Script を削除するエディタ拡張
+    /// </summary>
+    [InitializeOnLoad]
+    internal static class AutoRemoveMissingScript
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        static AutoRemoveMissingScript()
+        {
+            // シーンが保存された時に呼び出されます
+            EditorSceneManager.sceneSaving += ( scene, _ ) => Remove( scene.GetRootGameObjects() );
+
+            // シーンが閉じられる時に呼び出されます
+            EditorSceneManager.sceneClosing += ( scene, _ ) =>
+            {
+                if ( !Remove( scene.GetRootGameObjects() ) ) return;
+                EditorSceneManager.SaveScene( scene );
+            };
+
+            // プレハブのシーンが保存された時に呼び出されます
+            PrefabStage.prefabSaving += gameObject => Remove( gameObject );
+
+            // プレハブステージが閉じられる時に呼び出されます
+            PrefabStage.prefabStageClosing += stage =>
+            {
+                if ( !Remove( stage.prefabContentsRoot ) ) return;
+
+                var prefabStageType = typeof( PrefabStage );
+                var saveMethod      = prefabStageType.GetMethod( "Save", BindingFlags.Instance | BindingFlags.NonPublic );
+
+                Debug.Assert( saveMethod != null, nameof( saveMethod ) + " != null" );
+
+                saveMethod.Invoke( stage, Array.Empty<object>() );
+            };
+
+            // Unity の再生状態が変更された時に呼び出されます
+            EditorApplication.playModeStateChanged += change =>
+            {
+                if ( change != PlayModeStateChange.ExitingEditMode ) return;
+                Remove( SceneManager.GetActiveScene().GetRootGameObjects() );
+            };
+        }
+
+        /// <summary>
+        /// Missing Script を削除します
+        /// </summary>
+        private static bool Remove( params GameObject[] rootGameObjects )
+        {
+            var gameObjectArray = rootGameObjects
+                    .Where( x => 0 < GameObjectUtility.GetMonoBehavioursWithMissingScriptCount( x ) )
+                    .ToArray()
+                ;
+
+            if ( gameObjectArray.Length <= 0 ) return false;
+
+            // 保存時に Undo できるようにすると
+            // シーン保存後にも Dirty フラグが付いてしまうため
+            // Undo には登録しないようにしています
+
+            foreach ( var gameObject in gameObjectArray )
+            {
+                var isPartOfPrefabInstance = PrefabUtility.IsPartOfPrefabInstance( gameObject );
+
+                // 通常のシーンの Hierarchy において
+                // プレハブのインスタンスであれば対象外
+                if ( isPartOfPrefabInstance ) continue;
+
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript( gameObject );
+            }
+
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.AutoRemoveMissingScript
+# Kogane Auto Remove Missing Script
+
+シーンを保存する時や Unity を再生する時に Missing Script を削除するエディタ拡張
