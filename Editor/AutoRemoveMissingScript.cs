4cf1e012597a33fae65244e11cbf61e516a0e370
baba-s <shotababa1206@gmail.com>
2022-09-03 10:57:55 +0900

    fix

--- Editor/AutoRemoveMissingScript.cs
+++ Editor/AutoRemoveMissingScript.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,8 +12,24 @@ namespace Kogane.Internal
     /// シーンを保存する時や Unity を再生する時に Missing Script を削除するエディタ拡張
     /// </summary>
     [InitializeOnLoad]
-    internal static class AutoRemoveMissingScript
+    internal sealed class AutoRemoveMissingScript : IProcessSceneWithReport
     {
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        public int callbackOrder => 0;
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// シーンがビルドされる時に呼び出されます
+        /// </summary>
+        void IProcessSceneWithReport.OnProcessScene( Scene scene, BuildReport report )
+        {
+            Remove( scene.GetRootGameObjects() );
+        }
+
         //================================================================================
         // 関数(static)
         //================================================================================
@@ -23,32 +41,8 @@ namespace Kogane.Internal
             // シーンが保存された時に呼び出されます
             EditorSceneManager.sceneSaving += ( scene, _ ) => Remove( scene.GetRootGameObjects() );
 
-            // シーンの変更を破棄しようとした時に勝手に保存されてしまうため
-            // シーンが閉じられる時は無視しています
-            // // シーンが閉じられる時に呼び出されます
-            // EditorSceneManager.sceneClosing += ( scene, _ ) =>
-            // {
-            //     if ( !Remove( scene.GetRootGameObjects() ) ) return;
-            //     EditorSceneManager.SaveScene( scene );
-            // };
-
             // プレハブのシーンが保存された時に呼び出されます
             PrefabStage.prefabSaving += gameObject => Remove( gameObject );
-
-            // プレハブステージの変更を破棄しようとした時に勝手に保存されてしまうため
-            // プレハブステージが閉じられる時は無視しています
-            // // プレハブステージが閉じられる時に呼び出されます
-            // PrefabStage.prefabStageClosing += stage =>
-            // {
-            //     if ( !Remove( stage.prefabContentsRoot ) ) return;
-            //
-            //     var prefabStageType = typeof( PrefabStage );
-            //     var saveMethod      = prefabStageType.GetMethod( "Save", BindingFlags.Instance | BindingFlags.NonPublic );
-            //
-            //     Debug.Assert( saveMethod != null, nameof( saveMethod ) + " != null" );
-            //
-            //     saveMethod.Invoke( stage, Array.Empty<object>() );
-            // };
 
             // Unity の再生状態が変更された時に呼び出されます
             EditorApplication.playModeStateChanged += change =>
@@ -61,16 +55,19 @@ namespace Kogane.Internal
         /// <summary>
         /// Missing Script を削除します
         /// </summary>
-        private static bool Remove( params GameObject[] rootGameObjects )
+        private static void Remove( params GameObject[] rootGameObjects )
         {
             var gameObjectArray = rootGameObjects
                     .SelectMany( x => x.GetComponentsInChildren<Transform>( true ) )
                     .Select( x => x.gameObject )
                     .Where( x => 0 < GameObjectUtility.GetMonoBehavioursWithMissingScriptCount( x ) )
+                    // 通常のシーンの Hierarchy において
+                    // プレハブのインスタンスであれば対象外
+                    .Where( x => !PrefabUtility.IsPartOfPrefabInstance( x ) )
                     .ToArray()
                 ;
 
-            if ( gameObjectArray.Length <= 0 ) return false;
+            if ( gameObjectArray.Length <= 0 ) return;
 
             // 保存時に Undo できるようにすると
             // シーン保存後にも Dirty フラグが付いてしまうため
@@ -78,16 +75,8 @@ namespace Kogane.Internal
 
             foreach ( var gameObject in gameObjectArray )
             {
-                var isPartOfPrefabInstance = PrefabUtility.IsPartOfPrefabInstance( gameObject );
-
-                // 通常のシーンの Hierarchy において
-                // プレハブのインスタンスであれば対象外
-                if ( isPartOfPrefabInstance ) continue;
-
                 GameObjectUtility.RemoveMonoBehavioursWithMissingScript( gameObject );
             }
-
-            return true;
         }
     }
 }
